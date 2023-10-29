70e181de455634013dc708d2181938710015c591
AshokB <Ashok.Balusu@HCAHealthcare.com>
2023-10-29 13:46:09 -0500

    Workflow updates

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "gcs-gcs-test" {
   # provider = google.tf-pv1
 
-  name          = "gcp-gcs-test1-20231022"
+  name          = "gcp-gcs-test1-20231029"
   location      = "US"
   force_destroy = true
 
